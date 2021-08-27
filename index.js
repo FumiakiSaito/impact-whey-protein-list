cb34114af2fce0780217efc2f75d821cf8df6ac3
FumiakiSaito <haratairani3000points@gmail.com>
2021-08-28 01:45:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const puppeteer = require("puppeteer");
     );
     const datalist = [];
     for (let i = 0; i < dropdown.options.length; i++) {
-      datalist.push(dropdown.options[i].innerText);
+      datalist.push(dropdown.options[i].text);
     }
     return datalist;
   });
@@ -33,23 +33,42 @@ const puppeteer = require("puppeteer");
   });
   console.log(valueList);
 
-  // ドロップダウンを変更してまわる
   for (let value of valueList) {
+    // フレーバードロップダウン選択
     await page.select("#athena-product-variation-dropdown-5", value);
-    //await page.waitForTimeout(1000);
+
+    await page.waitForTimeout(1000);
+
+    // 容量ボタンクリック
+    await page.click(
+      "#mainContent > div.athenaProductPage_topRow > div.athenaProductPage_lastColumn > div > div.athenaProductPage_productVariations > div > div > div:nth-child(2) > div > div > ul > li:nth-child(1) > button"
+    );
+
+    //await page.screenshot({ path: "screenshot/" + value + ".png" });
+
     const price = await page.evaluate(() => {
+      // フレーバー名取得
+      const fravar = document.querySelector(
+        "#athena-product-variation-dropdown-5"
+      );
+      const fravarText = fravar.options[fravar.selectedIndex].text;
+
+      // 容量テキスト取得
+      const button1 = document.querySelector(
+        "#mainContent > div.athenaProductPage_topRow > div.athenaProductPage_lastColumn > div > div.athenaProductPage_productVariations > div > div > div:nth-child(2) > div > div > ul > li:nth-child(1) > button"
+      );
+      const capacityText = button1.innerText
+        .replace(/\r?\n/g, "")
+        .replace(/選択した/g, "");
+
+      // 値段取得
       const price = document.querySelector("p.productPrice_price");
-      return price.innerText;
+      const priceText = price.innerText;
+
+      return fravarText + "," + capacityText + "," + priceText;
     });
     console.log(price);
   }
-
-  // TODO: 容量変更
-
-  //await page.click(
-  //  "#mainContent > div.athenaProductPage_topRow > div.athenaProductPage_lastColumn > div > div.athenaProductPage_productVariations > div > div > div:nth-child(2) > div > div > ul > li:nth-child(1) > button"
-  //);
-  //await page.screenshot({ path: "test.png" });
 
   await browser.close();
 })();
