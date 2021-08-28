5df3ffc1f64e4ee0f473081f0c767cb04223944c
FumiakiSaito <haratairani3000points@gmail.com>
2021-08-28 10:44:07 +0900

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const puppeteer = require("puppeteer");
     "https://www.myprotein.jp/sports-nutrition/impact-whey-protein/10530943.html"
   );
 
-  // フレーバー一覧
+  // フレーバー名の一覧
+  /*
   const fravarList = await page.evaluate(() => {
     const dropdown = document.getElementById(
       "athena-product-variation-dropdown-5"
@@ -19,8 +20,9 @@ const puppeteer = require("puppeteer");
     return datalist;
   });
   console.log(fravarList);
+  */
 
-  // 値一覧
+  // フレーバーのオプション値リストを取得
   const valueList = await page.evaluate(() => {
     const dropdown = document.getElementById(
       "athena-product-variation-dropdown-5"
@@ -31,44 +33,69 @@ const puppeteer = require("puppeteer");
     }
     return datalist;
   });
-  console.log(valueList);
+  //console.log(valueList);
 
   for (let value of valueList) {
-    // フレーバードロップダウン選択
-    await page.select("#athena-product-variation-dropdown-5", value);
+    let detail = {
+      fravarText: null,
+      capacityText: null,
+      priceText: null,
+    };
 
+    // フレーバー選択
+    await page.select("#athena-product-variation-dropdown-5", value);
     await page.waitForTimeout(1000);
 
-    // 容量ボタンクリック
-    await page.click(
-      "#mainContent > div.athenaProductPage_topRow > div.athenaProductPage_lastColumn > div > div.athenaProductPage_productVariations > div > div > div:nth-child(2) > div > div > ul > li:nth-child(1) > button"
-    );
+    // 容量は250g, 1kg, 2.5kg, 5kgで最大4つのボタンがある。
+    // 在庫がない場合はボタン自体存在しないため、
+    // 存在チェックを行い存在したらボタンをクリックする
+    for (let i = 1; i <= 4; i++) {
+      // 容量ボタンのセレクタ
+      const capacitySelector =
+        "#mainContent > div.athenaProductPage_topRow > div.athenaProductPage_lastColumn > div > div.athenaProductPage_productVariations > div > div > div:nth-child(2) > div > div > ul > li:nth-child(" +
+        i +
+        ") > button";
 
-    //await page.screenshot({ path: "screenshot/" + value + ".png" });
+      // 容量ボタンの存在チェック
+      const existsCapacity = await page
+        .$(capacitySelector)
+        .then((res) => !!res);
+      if (existsCapacity) {
+        // 容量ボタンクリック
+        await page.click(capacitySelector);
+        await page.waitForTimeout(1000);
 
-    const price = await page.evaluate(() => {
-      // フレーバー名取得
-      const fravar = document.querySelector(
-        "#athena-product-variation-dropdown-5"
-      );
-      const fravarText = fravar.options[fravar.selectedIndex].text;
+        // 容量テキスト取得 (ex. 5 kg)
+        const capacityText = await page.$eval(
+          capacitySelector,
+          (el) => el.textContent
+        );
+        detail.capacityText = capacityText
+          .replace(/\r?\n/g, "")
+          .replace(/選択した/g, "");
 
-      // 容量テキスト取得
-      const button1 = document.querySelector(
-        "#mainContent > div.athenaProductPage_topRow > div.athenaProductPage_lastColumn > div > div.athenaProductPage_productVariations > div > div > div:nth-child(2) > div > div > ul > li:nth-child(1) > button"
-      );
-      const capacityText = button1.innerText
-        .replace(/\r?\n/g, "")
-        .replace(/選択した/g, "");
+        //await page.screenshot({ path: "screenshot/" + value + ".png" });
 
-      // 値段取得
-      const price = document.querySelector("p.productPrice_price");
-      const priceText = price.innerText;
+        // フレーバー名取得 (ex. ソルティッドキャラメル)
+        const fravarText = await page.evaluate(() => {
+          const fravar = document.querySelector(
+            "#athena-product-variation-dropdown-5"
+          );
+          const fravarText = fravar.options[fravar.selectedIndex].text;
+          return fravarText;
+        });
+        detail.fravarText = fravarText;
 
-      return fravarText + "," + capacityText + "," + priceText;
-    });
-    console.log(price);
+        // 価格取得 (ex. ¥7,390)
+        const priceText = await page.evaluate(() => {
+          const price = document.querySelector("p.productPrice_price");
+          const priceText = price.innerText;
+          return priceText;
+        });
+        detail.priceText = priceText;
+        console.log(detail);
+      }
+    }
   }
-
   await browser.close();
 })();
